d813685725b4fa81b16971798a240cf6a09aeeaf
Reiner Terry <dragoreiner@gmail.com>
2024-10-09 14:24:37 +0700

    zustand implementation

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,7 @@
 export const NEXT_PUBLIC_APP_ENV = process.env.NEXT_PUBLIC_APP_ENV as string;
+export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development' && process.env.APP_ENV === 'development';
+export const IS_TEST = process.env.NODE_ENV === 'test' || process.env.APP_ENV === 'test';
+export const IS_PRODUCTION = process.env.NODE_ENV === 'production' && process.env.APP_ENV === 'production';
+
+export const IS_BROWSER = typeof window !== 'undefined';
+export const IS_SERVER = typeof window === 'undefined';
---
+++ src/translations/ni18n.config.ts
@@ -0,0 +1,42 @@
+import LanguageDetector from 'i18next-browser-languagedetector';
+import ChainedBackend from 'i18next-chained-backend';
+import HttpBackend from 'i18next-http-backend';
+import LocalStorageBackend from 'i18next-localstorage-backend';
+import type { Ni18nOptions } from 'ni18n';
+
+import {
+  IS_BROWSER,
+  IS_DEVELOPMENT,
+} from '@/config/constants';
+
+export const ni18nConfig: Ni18nOptions = {
+  supportedLngs: ['id', 'en'],
+  fallbackLng: 'en',
+  ns: [
+    'common',
+    'home',
+    'notification',
+    'master',
+    'menu',
+  ],
+  use: IS_BROWSER
+    ? [ChainedBackend, LanguageDetector]
+    : undefined,
+  interpolation: {
+    escapeValue: false,
+  },
+  debug: IS_DEVELOPMENT,
+  backend: IS_BROWSER
+    ? {
+        backends: [LocalStorageBackend, HttpBackend],
+        backendOptions: [
+          {
+            expirationTime: 12 * 60 * 60 * 1000,
+          },
+          {
+            loadPath: '/locales/{{lng}}/{{ns}}.json',
+          },
+        ],
+      }
+    : undefined,
+};
