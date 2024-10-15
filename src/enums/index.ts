06afba06723ba120974cb3e7cbedbf8b0cd32b20
DRucci-88 <d.rucci.2001@gmail.com>
2024-10-15 17:23:07 +0700

    PascalCase enum

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -3,44 +3,44 @@ export enum MasterEnum {
   INACTIVE = 0,
 }
 
-export enum textFieldVariantEnum {
-  FILLED = 'filled',
-  OUTLINED = 'outlined',
-  STANDARD = 'standard',
+export enum TextFieldVariantEnum {
+  FILLED = "filled",
+  OUTLINED = "outlined",
+  STANDARD = "standard",
 }
 
-export enum buttonVariantEnum {
-  CONTAINED = 'contained',
-  OUTLINED = 'outlined',
-  TEXT = 'text',
+export enum ButtonVariantEnum {
+  CONTAINED = "contained",
+  OUTLINED = "outlined",
+  TEXT = "text",
 }
 
-export enum containerSizeEnum {
-  EXTRASMALL = 'xs',
-  SMALL = 'sm',
-  MEDIUM = 'md',
-  LARGE = 'lg',
-  EXTRALARGE = 'xl',
+export enum ContainerSizeEnum {
+  EXTRASMALL = "xs",
+  SMALL = "sm",
+  MEDIUM = "md",
+  LARGE = "lg",
+  EXTRALARGE = "xl",
 }
 
-export enum labelVariantEnum {
-  H1 = 'h1',
-  H2 = 'h2',
-  H3 = 'h3',
-  H4 = 'h4',
-  H5 = 'h5',
-  H6 = 'h6',
-  SUBTITLE1 = 'subtitle1',
-  SUBTITLE2 = 'subtitle2',
-  BODY1 = 'body1',
-  BODY2 = 'body2',
-  BUTTON = 'button',
-  CAPTION = 'caption',
+export enum LabelVariantEnum {
+  H1 = "h1",
+  H2 = "h2",
+  H3 = "h3",
+  H4 = "h4",
+  H5 = "h5",
+  H6 = "h6",
+  SUBTITLE1 = "subtitle1",
+  SUBTITLE2 = "subtitle2",
+  BODY1 = "body1",
+  BODY2 = "body2",
+  BUTTON = "button",
+  CAPTION = "caption",
 }
 
-export enum iconEnum {
-  SEARCH = 'search',
-  PLUS = 'add',
-  MINUS = 'remove',
-  PERCENT = 'percent',
+export enum IconEnum {
+  SEARCH = "search",
+  PLUS = "add",
+  MINUS = "remove",
+  PERCENT = "percent",
 }
