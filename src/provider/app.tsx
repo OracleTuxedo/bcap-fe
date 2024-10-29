a00645363f0ac5a9fb2cea70201197898741f59b
Reiner Terry <dragoreiner@gmail.com>
2024-10-29 16:02:45 +0700

    adapt erp-frontend

---
+++ src/provider/app.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+type AppProviderProps = {
+    children: ReactNode;
+};
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
