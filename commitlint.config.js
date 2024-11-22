02406efcbf778e65ac786c4c47d895617df50f68
DRucci-88 <d.rucci.2001@gmail.com>
2024-11-22 11:24:54 +0700

    feat: commintlint new rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  // rules: {
+  //   'type-empty': [2, 'never'], // Type must not be empty
+  //   'subject-empty': [2, 'never'], // Subject must not be empty
+  //   'scope-empty': [1, 'always'], // Scope can be optional (set to warning)
+  // },
 };
